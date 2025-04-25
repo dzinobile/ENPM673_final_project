4de96d00c63f8cc5e569ff0d512677517eee38e3
dzinobile <dzinobile@gmail.com>
2025-04-25 17:28:02 -0400

    created directional motion script

---
+++ build/CMakeFiles/animated_box.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/vboxuser/ENPM673_final_project/group2_ws/src/enpm673_final_proj/src/animated_box.cpp" "CMakeFiles/animated_box.dir/src/animated_box.cpp.o" "gcc" "CMakeFiles/animated_box.dir/src/animated_box.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/cpp_enpm673_final_proj.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for cpp_enpm673_final_proj.
+# This may be replaced when dependencies are built.
