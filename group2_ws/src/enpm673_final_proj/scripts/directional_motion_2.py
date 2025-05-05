a6cf539c6834cf4b50ea36b3e99e593de1ae2461
dzinobile <dzinobile@gmail.com>
2025-05-05 18:33:48 -0400

    commit

--- group2_ws/src/enpm673_final_proj/scripts/directional_motion_2.py
+++ group2_ws/src/enpm673_final_proj/scripts/directional_motion_2.py
@@ -7,6 +7,7 @@ from cv_bridge import CvBridge
 import cv2
 import numpy as np
 from geometry_msgs.msg import Twist
+import time
 
 from enpm673_module.enpm673_final_proj import *
 
@@ -19,36 +20,82 @@ class DirectionalMotion(Node):
             [  0.        ,   0.        ,   1.        ]])
 
         self.previous_z = 10000
+        self.previous_yaw = 0
         self.bridge = CvBridge()
-        self.raw_image_sub = self.create_subscription(Image,'/camera/image_raw',self.raw_image_callback,10)
+        self.raw_image_sub = self.create_subscription(Image,'/camera/image_raw',self.raw_image_callback,2)
         self.publisher_cmd_vel= self.create_publisher(Twist,'/cmd_vel', 10)
+        self.iterations = 0
+
 
     def move_robot(self,tvec,yaw):
         msg = Twist()
          # Extract forward and sideways distances (in camera frame)
         x = tvec[0][0]   # left-right (positive = right)
         z = tvec[0][2]   # forward distance (positive = forward)
-
+        linear_k = 0.1
+        msg.linear.x = linear_k * z
+        msg.linear.x = np.clip(msg.linear.x, -0.1, 0.1)
+        yaw = yaw-0.03
+        print(yaw)
+        angular_k = 0.5
+        msg.angular.z = -angular_k * yaw  # Rotate to align with marker long axis
+        msg.angular.z = np.clip(msg.angular.z,-0.5,0.5)
+        # if z > 1.5:
+        #     a = np.rad2deg(np.arctan(x/z))+25
+        #     if abs(a) > 2:
+        #             angular_k = 0.5
+        #             msg.angular.z = -angular_k*a
+        #             msg.angular.z = np.clip(msg.angular.z, -0.3, 0.3)
+        #             msg.linear.x = 0.0
+        #     else:
+        #         linear_k = 0.1
+        #         msg.linear.x = linear_k * z
+        #         msg.linear.x = np.clip(msg.linear.x, -0.1, 0.1)
+        #         msg.angular.z = 0.0
+        # else:
+        #     yaw = yaw+0.6
+        #     print(yaw)
+        #     msg.linear.x = 0.001
+        #     angular_k = 0.5
+        #     msg.angular.z = -angular_k * yaw  # Rotate to align with marker long axis
+        #     msg.angular.z = np.clip(msg.angular.z,-0.5,0.5)
+        
         # Basic control gains (tune these)
-        linear_k = 0.5
-        angular_k = 1.0
+        #linear_k = 0.1
+        #msg.linear.x = linear_k * z
+        #msg.linear.x = np.clip(msg.linear.x, -0.1, 0.1)
+
 
         # Desired linear and angular velocities
-        msg.linear.x = linear_k * z  # Drive forward toward marker
-        msg.angular.z = -angular_k * yaw  # Rotate to align with marker long axis
+          # Drive forward toward marker
+        # if z > 1:
+        #     angular_k = 0.1
+        #     msg.angular.z = -angular_k*a
+        #     msg.angular.z = np.clip(msg.angular.z, -0.1, 0.1)
+        # else:
+        #     angular_k = 0.5
+        #     msg.angular.z = -angular_k * yaw  # Rotate to align with marker long axis
+        #     msg.angular.z = np.clip(msg.angular.z,-0.5,0.5)
 
         # Optional: limit max speed
-        msg.linear.x = np.clip(msg.linear.x, -0.3, 0.3)
-        msg.angular.z = np.clip(msg.angular.z, -1.0, 1.0)
+        
+        
 
         # Publish the command
         self.publisher_cmd_vel.publish(msg)
-        print(msg)
-
-    # def turn_robot(self,yaw):
-    #     linear_k = 0.5
-    #     msg.angular.z = -angular_k * yaw  # Rotate to align with marker long axis
         
+
+    def turn_robot(self,yaw):
+        print("turn robot")
+        msg= Twist()
+        msg.angular.z = -0.5* yaw  # Rotate to align with marker long axis
+        self.publisher_cmd_vel.publish(msg)
+        time.sleep(2)
+
+    def stop_robot(self):
+        msg = Twist()
+        msg.linear.x = 0.0
+        self.publisher_cmd_vel.publish(msg)
 
     def raw_image_callback(self,msg):
     #def raw_image_callback(self,msg):
@@ -57,6 +104,7 @@ class DirectionalMotion(Node):
         dist_coeffs = np.zeros((5,))
         marker_length = 0.1
         
+
 
         #camera_matrix = np.array([[527.09681904, 0, 319.50217002],
                                   #[0, 30.38209677, 239.49903363],
@@ -90,9 +138,19 @@ class DirectionalMotion(Node):
                     x_axis_proj /= np.linalg.norm(x_axis_proj)
                     yaw = np.arctan2(x_axis_proj[0], x_axis_proj[2])
                     z = tvec[0][2]
-                    if z < self.previous_z:
-                        self.move_robot(closest_tvec,yaw)
-                        self.previous_z = z
+                    self.move_robot(closest_tvec,yaw)
+                    # print(z)
+                    # if z < self.previous_z+0.5:
+                    #     self.move_robot(closest_tvec,yaw)
+                    #     self.previous_z = z
+                    #     self.previous_yaw = yaw
+                    # else:
+                    #     self.stop_robot()
+                    #     self.turn_robot(self.previous_yaw)
+                    #     self.previous_z = z
+                    #     self.previous_yaw = yaw
+
+
 
 
             
